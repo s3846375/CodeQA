739e331af95c64de025057ce000c24a327969101
Wann-Ting Tu (s3846375) <S3846375@student.rmit.edu.au>
2024-05-17 01:39:04 +1000

    Software Engineering Assignment 4

---
+++ src/Numbers.java
@@ -0,0 +1,13 @@
+public class Numbers {
+
+    public int sumNumbers(int n1, int n2) {
+        return n1 + n2;
+    }
+
+    public boolean checkOddNumber(int n) {
+        if (n % 2 != 0)
+            return true;
+        else
+            return false;
+    }
+}
